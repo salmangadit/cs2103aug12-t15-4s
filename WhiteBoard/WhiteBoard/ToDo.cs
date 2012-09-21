603699e7c210a23272d711573c5aca72fc87810e
Sai <Sai@Sai-PC.PROLiNK>
2012-09-21 11:34:00 +0800

    added ToDo class; added more code to Command and AddCommand class

---
+++ WhiteBoard/WhiteBoard/ToDo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhiteBoard
+{
+    class ToDo
+    {
+        string taskDescription;
+
+        DateTime taskStartTime;
+        DateTime taskEndTime;
+
+        DateTime taskDeadline;
+
+        public string TaskDesciption
+        {
+            get
+            {
+                if (taskDescription == null)
+                {
+                    throw new ArgumentNullException("Task Description is not set");
+                }
+
+                return taskDescription;
+            }
+            set
+            {
+                this.taskDescription = value;
+            }
+        }
+
+        public DateTime StartTime
+        {
+            get
+            {
+                if (taskStartTime == null)
+                {
+                    throw new ArgumentNullException("Start time is not set");
+                }
+
+                return taskStartTime;
+            }
+            set
+            {
+                this.taskStartTime = value;
+            }
+        }
+
+        public DateTime EndTime
+        {
+            get
+            {
+                if (taskEndTime == null)
+                {
+                    throw new ArgumentNullException("Start time is not set");
+                }
+
+                return taskEndTime;
+            }
+            set
+            {
+                this.taskEndTime = value;
+            }
+        }
+
+        public DateTime Deadline
+        {
+            get
+            {
+                if (taskDeadline == null)
+                {
+                    throw new ArgumentNullException("Start time is not set");
+                }
+
+                return taskDeadline;
+            }
+            set
+            {
+                this.taskDeadline = value;
+            }
+        }
+    }
+}
