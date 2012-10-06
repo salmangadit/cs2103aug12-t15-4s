b93181494b36e3c7884d9dea06c9453b57dc55f1
Nizra <Nizra@Nizra-PC.nus.edu.sg>
2012-10-07 01:53:32 +0800

    Implemented a method for incrementing and storing TaskId in FileHandler class. Untested.

--- WhiteBoard/WhiteBoard/FileHandler.cs
+++ WhiteBoard/WhiteBoard/FileHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace WhiteBoard
@@ -31,6 +32,8 @@ namespace WhiteBoard
 
         internal void AddTaskToFile(Task taskToAdd)
         {
+            //int taskId;
+            int newTaskId;
             List<Task> listOfTasksToAdd = new List<Task>();
 
             XmlSerializer objXmlSer = new XmlSerializer(typeof(List<Task>));
@@ -50,6 +53,36 @@ namespace WhiteBoard
 
             // Clear list
             listOfTasksToAdd.Clear();
+
+            XmlDocument taskListDoc = new XmlDocument();
+
+            if (File.Exists(filePath))
+            {
+                taskListDoc.Load(filePath);
+                XmlElement rootElement = taskListDoc.DocumentElement; // Get reference to root node
+                XmlNodeList listOfTasks = taskListDoc.GetElementsByTagName("taskId"); // Create a list of nodes whose name is taskId
+
+                if (listOfTasks.Count > 0)
+                {
+                    int indexOfLastTask = listOfTasks.Count - 1;
+                    string lastTaskIdString = listOfTasks[indexOfLastTask].Value; // Get the value of the last task
+                    newTaskId = int.Parse(lastTaskIdString) + 1; // Increase value
+                }
+                else
+                {
+                    newTaskId = 1;
+                }
+
+                string newTaskIdString = newTaskId.ToString(); // Convert back to string
+
+                XmlElement newTaskElement = taskListDoc.CreateElement("taskId"); // Create element named taskId
+                newTaskElement.InnerText = newTaskIdString; // Specify text of new task element
+                XmlNodeList childrenOfTasks = rootElement.LastChild.ChildNodes;
+                rootElement.LastChild.InsertBefore(newTaskElement, childrenOfTasks[0]);
+                
+
+            }
+            
         }
 
         internal Task GetTaskFromFile(int editedTaskId)
