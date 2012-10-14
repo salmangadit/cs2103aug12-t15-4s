78ec19d20cb0e851acbb85cc8e9527b2d12d8a04
Nizra <Nizra@Nizra-PC.nus.edu.sg>
2012-10-14 18:09:49 +0800

    WhiteBoardDoc created

---
+++ WhiteBoard/WhiteBoard/SearchCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace WhiteBoard
+{
+    class SearchCommand : Command
+    {
+        string searchString;
+
+        public SearchCommand(FileHandler fileHandler, string searchString, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            this.searchString = searchString;
+            this.commandType = CommandType.Search;
+        }
+
+        public override CommandType CommandType
+        {
+            get
+            {
+                return commandType;
+            }
+        }
+
+        public override List<Task> Execute()
+        {
+            List<Task> listOfTasks = fileHandler.ViewAll();
+            List<Task> tasksContainingSearchString = new List<Task>();
+
+            for (int i = 0; i < listOfTasks.Count; i++)
+            {
+                if(listOfTasks[i].Description.Contains(searchString))
+                {
+                    tasksContainingSearchString.Add(listOfTasks[i]);
+                }
+            }
+
+            return tasksContainingSearchString;
+        }
+
+        public override List<Task> Undo()
+        {
+            return screenState;
+        }
+
+        public string GetSearchString()
+        {
+            return searchString;
+        }
+    }
+}
