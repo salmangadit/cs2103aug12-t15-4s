2745505386cde0bf7e07db665b9c8ce8eb79f81d
Salman <Salman@Salman-PC.nus.edu.sg>
2012-10-15 21:53:49 +0800

    Added end time

---
+++ WhiteBoard/WhiteBoard/EditCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace WhiteBoard
+{
+    class EditCommand : Command
+    {
+        Task editTaskDetails;
+        Task uneditedTask;
+
+        public EditCommand(FileHandler fileHandler, Task editTaskDetails, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            this.editTaskDetails = editTaskDetails;
+            this.commandType = CommandType.Edit;
+        }
+
+        public override CommandType CommandType
+        {
+            get
+            {
+                return commandType;
+            }
+        }
+
+        public override List<Task> Execute()
+        {
+            int editedTaskId = editTaskDetails.Id;
+            uneditedTask = fileHandler.GetTaskFromFile(editedTaskId);
+
+            string editedTaskDescription = (editTaskDetails.Description == null) ? uneditedTask.Description : editTaskDetails.Description;
+            DateTime? editedTaskStartTime = (editTaskDetails.StartTime == null) ? uneditedTask.StartTime : editTaskDetails.StartTime;
+            DateTime? editedTaskEndTime = (editTaskDetails.EndTime == null) ? uneditedTask.EndTime : editTaskDetails.EndTime;
+            DateTime? editedTaskDeadline = (editTaskDetails.Deadline == null) ? uneditedTask.Deadline : editTaskDetails.Deadline;
+
+            Task editedTask = new Task(editedTaskId, editedTaskDescription, editedTaskStartTime, editedTaskEndTime, editedTaskDeadline);
+
+            fileHandler.WriteEditedTaskToFile(editedTask);
+
+            List<Task> editedTasks = new List<Task>();
+            editedTasks.Add(editedTask);
+
+            return editedTasks;
+        }
+
+        public override List<Task> Undo()
+        {
+            fileHandler.WriteEditedTaskToFile(uneditedTask);
+            return screenState;
+        }
+    }
+}
---
+++ WhiteBoard/WhiteBoard/ViewCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace WhiteBoard
+{
+    class ViewCommand : Command
+    {
+        Task tasksToView;
+
+        public ViewCommand(FileHandler fileHandler, Task viewTaskDetails, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            this.tasksToView = viewTaskDetails;
+            this.commandType = CommandType.View;
+        }
+
+        public override CommandType CommandType
+        {
+            get
+            {
+                return commandType;
+            }
+        }
+
+        public override List<Task> Execute()
+        {
+            if (tasksToView.Archive == true)
+            {
+                return fileHandler.ViewArchive();
+            }
+            else if (tasksToView.Archive == false && tasksToView.Deadline == null && tasksToView.StartTime == null && tasksToView.EndTime == null)
+            {
+                return fileHandler.ViewAll();
+            }
+            else if (tasksToView.Archive == false && tasksToView.Deadline == null)
+            {
+                return fileHandler.ViewTasks(tasksToView.StartTime, tasksToView.EndTime);
+            }
+            else if (tasksToView.Archive == false && tasksToView.StartTime == null && tasksToView.EndTime == null)
+            {
+                return fileHandler.ViewTasks(tasksToView.Deadline);
+            }
+            else
+            {
+                throw new NotImplementedException("There is no such criteria for file viewing!");
+            }
+
+            return null;
+        }
+
+        public override List<Task> Undo()
+        {
+            return screenState;
+        }
+    }
+}
