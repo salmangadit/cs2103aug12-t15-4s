0edec9f68cbe398ef46f1094ebb58b988bf6d449
Sai <Sai@Sai-PC.nus.edu.sg>
2012-10-21 17:42:04 +0800

    done autocomplete functionality observer pattern; subject: FileHandler, observer: Autocomplete

---
+++ WhiteBoard/WhiteBoard/ArchiveCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace WhiteBoard
+{
+    class ArchiveCommand : Command
+    {
+        int taskIdToArchive;
+
+        public ArchiveCommand(FileHandler fileHandler, int taskIdToArchive, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            this.taskIdToArchive = taskIdToArchive;
+            this.commandType = CommandType.Archive;
+        }
+
+        public override CommandType CommandType
+        {
+            get
+            {
+                return commandType;
+            }
+        }
+
+        public override List<Task> Execute()
+        {
+            bool isTaskArchived = fileHandler.ArchiveTaskInFile(taskIdToArchive);
+
+            if (!isTaskArchived)
+                throw new ApplicationException("Unable To Archive Task");
+
+            return null;
+        }
+
+        public override List<Task> Undo()
+        {
+            fileHandler.UnarchiveTaskInFile(taskIdToArchive);
+            return screenState;
+        }
+
+        public int GetArchivedTaskId()
+        {
+            return taskIdToArchive;
+        }
+    }
+}
---
+++ WhiteBoard/WhiteBoard/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace WhiteBoard
+{
+    class Controller
+    {
+        FileHandler fileHandler;
+        Stack<Command> history;
+
+        public Controller()
+        {
+            fileHandler = FileHandler.Instance;
+            history = new Stack<Command>();
+        }
+
+        public Command GetCommandObject(string userString, List<Task> screenState)
+        {
+            CommandParser commandParser = new CommandParser(fileHandler, screenState, history);
+            return commandParser.ParseCommand(userString);
+        }
+
+        public Command GetAllTasks(List<Task> screenState)
+        {
+            CommandParser commandParser = new CommandParser(fileHandler, screenState, history);
+            return commandParser.ParseCommand("VIEW ALL");
+        }
+    }
+}
