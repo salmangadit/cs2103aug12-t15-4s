d451968f7a15ef9c9134f717b7858c6e8d5f9e3e
Sai <Sai@Sai-PC.comp.nus.edu.sg>
2012-10-21 23:39:47 +0800

    comments for refactoring

--- WhiteBoard/WhiteBoard/CommandType.cs
+++ WhiteBoard/WhiteBoard/CommandType.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 namespace WhiteBoard
 {
+    // remove this class when you refactor, better to use
+    // object is Class to check, faster and cleaner
     public enum CommandType
     {
         Add,
