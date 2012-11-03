ab82376a592ae0dc98704b900cfb6cba221445c4
Nizra <Nizra@Nizra-PC.nus.edu.sg>
2012-11-03 23:07:40 +0800

    Modified view tasks method

---
+++ WhiteBoard/WhiteBoard/CommandHistory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhiteBoard
+{
+    class CommandHistory
+    {
+        List<string> commandHistory;
+        int currIndex = -1;
+
+        public CommandHistory()
+        {
+            commandHistory = new List<string>();
+        }
+
+        public void AddToHistory(string command)
+        {
+            commandHistory.Add(command);
+            currIndex = -1;
+        }
+
+        public string UpClick()
+        {
+            if (commandHistory.Count == 0)
+                return null;
+
+            if (currIndex == -1)
+                currIndex = commandHistory.Count - 1;
+            else if (currIndex == 0)
+                return null;
+            else
+                currIndex -= 1;
+
+            return commandHistory[currIndex];
+        }
+
+        public string DownClick()
+        {
+            if (commandHistory.Count == 0)
+                return null;
+
+            if (currIndex == -1)
+                return null;
+            else if (currIndex == commandHistory.Count - 1)
+                return null;
+            else
+                currIndex += 1;
+
+            return commandHistory[currIndex];
+        }
+    }
+}
