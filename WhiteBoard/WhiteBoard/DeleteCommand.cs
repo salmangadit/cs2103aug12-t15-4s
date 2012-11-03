7e6f916516ede53926f66d89b87655af0e812811
Nizra <Nizra@Nizra-PC.nus.edu.sg>
2012-11-03 19:36:10 +0800

    Made addTaskToFile methods boolean

---
+++ WhiteBoard/WhiteBoard/DeleteCommand.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace WhiteBoard
+{
+    class DeleteCommand : Command
+    {
+        List<int> taskIdsToDelete;
+        List<Task> tasksToDelete;
+
+        public DeleteCommand(FileHandler fileHandler, int taskIdToDelete, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            taskIdsToDelete = new List<int>();
+            tasksToDelete = new List<Task>();
+            this.taskIdsToDelete.Add(taskIdToDelete);
+            this.commandType = CommandType.Delete;
+        }
+
+        public DeleteCommand(FileHandler fileHandler, List<int> taskIdsToDelete, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            taskIdsToDelete = new List<int>();
+            tasksToDelete = new List<Task>();
+            this.taskIdsToDelete = taskIdsToDelete;
+            this.commandType = CommandType.Delete;
+        }
+
+        public DeleteCommand(FileHandler fileHandler, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            taskIdsToDelete = new List<int>();
+            tasksToDelete = new List<Task>();
+            foreach (Task task in screenState)
+            {
+                this.taskIdsToDelete.Add(task.Id);
+            }
+            this.commandType = CommandType.Delete;
+        }
+
+        public override CommandType CommandType
+        {
+            get
+            {
+                return commandType;
+            }
+        }
+
+        public override List<Task> Execute()
+        {
+            if (taskIdsToDelete.Count == 0)
+            {
+                throw new ApplicationException("No tasks to delete");
+            }
+
+            foreach (int taskIdToDelete in taskIdsToDelete)
+            {
+                Debug.Assert(taskIdToDelete > 0, "Invalid task ID");
+
+                bool isTaskDeleted;
+
+                try
+                {
+                    tasksToDelete.Add(fileHandler.GetTaskFromFile(taskIdToDelete));
+                    isTaskDeleted = fileHandler.DeleteTaskFromFile(taskIdToDelete);
+                }
+                catch (SystemException e)
+                {
+                    Log.Debug("Caught a System Exception" + e);
+                    throw new ApplicationException("There are no tasks to delete");
+                }
+
+                if (isTaskDeleted)
+                {
+                    Log.Debug("Delete Command was executed for" + taskIdToDelete);
+                }
+                else
+                {
+                    Log.Debug("Delete Command failed for" + taskIdToDelete);
+                    throw new ApplicationException("Unable To Delete Task with ID T" + taskIdToDelete);
+                }
+            }
+
+            return tasksToDelete;
+        }
+
+        public override List<Task> Undo()
+        {
+            foreach (Task task in tasksToDelete)
+            {
+                Debug.Assert(task.Id > 0, "Invalid task ID");
+                fileHandler.AddTaskToFile(task, task.Id);
+                Log.Debug("Delete Command was undone for" + task.Id);
+            }
+            return screenState;
+        }
+
+        public List<int> GetDeletedTaskId()
+        {
+            return taskIdsToDelete;
+        }
+    }
+}
