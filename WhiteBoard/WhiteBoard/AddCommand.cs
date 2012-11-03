adbe9d6a8f8fe85010427bc27d78cb9658dc2d12
Nizra <Nizra@Nizra-PC.nus.edu.sg>
2012-11-03 20:39:05 +0800

    Added sort

---
+++ WhiteBoard/WhiteBoard/AddCommand.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using log4net;
+
+namespace WhiteBoard
+{
+    class AddCommand : Command
+    {
+        Task taskToAdd;
+        List<Task> addedTask;
+
+        public AddCommand(FileHandler fileHandler, Task taskToAdd, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            addedTask = new List<Task>();
+            this.taskToAdd = taskToAdd;
+            this.commandType = CommandType.Add;
+        }
+
+        public override CommandType CommandType
+        {
+            get
+            {
+                return commandType;
+            }
+        }
+
+        public override List<Task> Execute()
+        {
+            Debug.Assert(taskToAdd != null, "Task to add is null");
+
+            if (taskToAdd.StartTime == null)
+            {
+                if (taskToAdd.EndTime != null)
+                {
+                    throw new ApplicationException("Task can't have an end time without start time");
+                }
+            }
+
+            if (!isFloatingTask(taskToAdd) && (((DateTime)taskToAdd.StartTime).DayOfYear < DateTime.Now.DayOfYear))
+            {
+                throw new ApplicationException("Task cannot start in the past");
+            }
+
+            if (!isFloatingTask(taskToAdd) && (taskToAdd.StartTime > taskToAdd.EndTime))
+            {
+                throw new ApplicationException("Task cannot begin after it ends!");
+            }
+
+            if (taskToAdd.Description.Equals(String.Empty))
+            {
+                throw new ApplicationException("Please provide a task description");
+            }
+
+            bool isTaskAdded = fileHandler.AddTaskToFile(taskToAdd);
+            addedTask.Add(taskToAdd);
+
+            if (isTaskAdded)
+            {
+                Log.Debug("Add Command was executed for" + taskToAdd.Id);
+            }
+            else
+            {
+                Log.Debug("Add Command failed for" + taskToAdd.Id);
+                throw new ApplicationException("Unable To Add Task with ID T" + taskToAdd.Id);
+            }
+
+            return addedTask;
+        }
+
+        private bool isFloatingTask(Task taskToAdd)
+        {
+            if ((taskToAdd.StartTime == null || taskToAdd.EndTime == null) && !(taskToAdd.StartTime == null && taskToAdd.EndTime != null))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override List<Task> Undo()
+        {
+            Debug.Assert(addedTask[0] != null, "No task in added task list");
+
+            if (fileHandler.DeleteTaskFromFile(addedTask[0].Id))
+            {
+                Log.Debug("Add Command Undone for task" + addedTask[0].Id);
+                return screenState;
+            }
+            else
+            {
+                Log.Debug("Add Command Undo Failed for task" + addedTask[0].Id);
+                throw new SystemException("Add Command Undo Failed for task" + addedTask[0].Id);
+            }
+        }
+    }
+}
---
+++ WhiteBoard/WhiteBoard/AutoComplete.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using log4net;
+
+namespace WhiteBoard
+{
+    /// <summary>
+    /// Interaction logic for AutoComplete.xaml
+    /// </summary>
+    public partial class AutoComplete : UserControl
+    {
+        List<String> autoCompleteList;
+        public event KeyEventHandler AutoCompleteKeyboardEvent = delegate { };
+        public event MouseEventHandler AutoCompleteMouseEvent = delegate { };
+
+        private string selectedItem = null;
+
+        protected static readonly ILog log = LogManager.GetLogger(typeof(AutoComplete));
+
+        public int Count
+        {
+            get
+            {
+                if (autoCompleteList == null)
+                    return 0;
+                else
+                    return autoCompleteList.Count();
+            }
+        }
+
+        public string SelectedItem
+        {
+            get
+            {
+                if (selectedItem != null)
+                    return selectedItem;
+                else
+                    return null;
+            }
+        }
+
+        public AutoComplete()
+        {
+            InitializeComponent();
+            autoCompleteList = new List<string>();
+            lstAutoComplete.DataContext = autoCompleteList;
+            lstAutoComplete.ItemsSource = autoCompleteList;
+        }
+
+        public void Show(List<string> searchResults)
+        {
+            log.Debug("Showing Auto Complete List");
+            if (searchResults == null)
+                throw new NullReferenceException();
+
+            autoCompleteList = searchResults;
+            lstAutoComplete.DataContext = autoCompleteList;
+            lstAutoComplete.ItemsSource = autoCompleteList;
+            lstAutoComplete.Items.Refresh();
+
+            if (autoCompleteList == null)
+                return;
+
+            if (autoCompleteList.Count > 0)
+            {
+                if (autoCompleteList.Count > 10)
+                {
+                    this.Height = 150;
+                }
+                else
+                {
+                    this.Height = autoCompleteList.Count * 18;
+                }
+            }
+        }
+
+        private void UserControl_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (lstAutoComplete.Items.Count > 0)
+            {
+                log.Debug("Focusing on Auto Complete List");
+                ListBoxItem item = (ListBoxItem)lstAutoComplete.ItemContainerGenerator.ContainerFromIndex(0);
+                FocusManager.SetFocusedElement(lstAutoComplete, item);
+            }
+        }
+
+        private void lstAutoComplete_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (lstAutoComplete.SelectedItem == null)
+                    return;
+
+                log.Debug("Selecting item from AutoComplete");
+                this.selectedItem = lstAutoComplete.SelectedItem.ToString();
+                AutoCompleteKeyboardEvent(this, e);
+            }
+        }
+
+        private void lstAutoComplete_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (lstAutoComplete.SelectedItem == null)
+                return;
+            log.Debug("Selecting item from AutoComplete");
+            this.selectedItem = lstAutoComplete.SelectedItem.ToString();
+            AutoCompleteMouseEvent(this, e);
+        }
+
+
+    }
+}
