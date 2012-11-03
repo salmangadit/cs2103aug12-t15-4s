989fc125ea67af62292afe2fb6b6e6f6a1faeb3b
Salman <Salman@Salman-PC>
2012-11-04 00:27:56 +0800

    Fixed undo defect

---
+++ WhiteBoard/WhiteBoard/UpdateType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhiteBoard
+{
+    enum UpdateType
+    {
+        Add,
+        Delete,
+        Archive,
+        Edit,
+        Unarchive
+    }
+}
