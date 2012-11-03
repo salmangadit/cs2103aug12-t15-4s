c321f17abe8db74aeb0627bfb4be9b1ecd7e5c9c
Salman <Salman@Salman-PC>
2012-11-03 23:36:18 +0800

    Fixed toast bugs

---
+++ WhiteBoard/WhiteBoard/Toast.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Windows;
+using log4net;
+
+namespace WhiteBoard
+{
+    class Toast
+    {
+        TextBlock lblToast;
+        bool enter;
+        int toastTime;
+        DispatcherTimer toastTimer, toastAnimationTimer;
+        protected static readonly ILog log = LogManager.GetLogger(typeof(Toast));
+
+        public Toast(TextBlock lblToast)
+        {
+            this.lblToast = lblToast;
+        }
+
+        public void ShowToast(string toast)
+        {
+            log.Debug("Showing toast...");
+            toastTimer = new System.Windows.Threading.DispatcherTimer();
+            toastTimer.Tick += new EventHandler(toastTimer_Tick);
+            toastTimer.Interval = new TimeSpan(0, 0, 3);
+            toastTimer.Start();
+
+            toastAnimationTimer = new DispatcherTimer();
+            toastAnimationTimer.Tick += new EventHandler(toastAnimationTimer_Tick);
+            toastAnimationTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            toastAnimationTimer.Start();
+            enter = true;
+            toastTime = 0;
+
+            lblToast.Text = toast;
+            if (toast.Length * 10 > 400)
+            {
+                lblToast.Width = 400;
+            }
+            else
+            {
+                lblToast.Width = toast.Length * 10;
+            }
+            lblToast.Visibility = Visibility.Visible;
+            lblToast.Opacity = 0;
+        }
+
+        private void toastTimer_Tick(object sender, EventArgs e)
+        {
+            lblToast.Visibility = Visibility.Collapsed;
+            toastTimer.Stop();
+        }
+
+        private void toastAnimationTimer_Tick(object sender, EventArgs e)
+        {
+            toastTime++;
+            if (lblToast.Opacity < 1 && enter)
+            {
+                if (lblToast.Opacity >= 0.8)
+                {
+                    enter = false;
+                }
+                lblToast.Opacity += 0.2;
+            }
+            else if (toastTime >= 15)
+            {
+                if (lblToast.Opacity <= 0.2)
+                {
+                    log.Debug("Hiding toast...");
+                    toastAnimationTimer.Stop();
+                }
+                lblToast.Opacity -= 0.2;
+            }
+        }
+    }
+}
---
+++ WhiteBoard/WhiteBoard/ViewCommand.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace WhiteBoard
+{
+    class ViewCommand : Command
+    {
+        Task viewTaskDetails;
+
+        public ViewCommand(FileHandler fileHandler, Task viewTaskDetails, List<Task> screenState)
+            : base(fileHandler, screenState)
+        {
+            this.viewTaskDetails = viewTaskDetails;
+            this.commandType = CommandType.View;
+        }
+
+        public override CommandType CommandType
+        {
+            get
+            {
+                return commandType;
+            }
+        }
+
+        public override List<Task> Execute()
+        {
+            Debug.Assert(viewTaskDetails != null, "Task Details not set");
+
+            List<Task> tasksToView = new List<Task>();
+
+            if (viewTaskDetails.Archive == true)
+            {
+                Log.Debug("View archived tasks");
+                tasksToView = fileHandler.ViewArchive();
+            }
+            else if (viewTaskDetails.Archive == false && viewTaskDetails.StartTime == null && viewTaskDetails.EndTime == null)
+            {
+                Log.Debug("View all tasks");
+                tasksToView = fileHandler.ViewAll();
+            }
+            else if (viewTaskDetails.Archive == false && viewTaskDetails.EndTime == null)
+            {
+                Log.Debug("View tasks ending on" + viewTaskDetails.StartTime);
+                tasksToView = fileHandler.ViewTasks(viewTaskDetails.StartTime);
+            }
+            else if (viewTaskDetails.Archive == false)
+            {
+                Log.Debug(String.Format("View tasks starting from {0} and ending on {1}", viewTaskDetails.StartTime, viewTaskDetails.EndTime));
+                tasksToView = fileHandler.ViewTasks(viewTaskDetails.StartTime, viewTaskDetails.EndTime);
+            }
+            else
+            {
+                Log.Debug("Invalid condition for Viewing Tasks");
+                throw new NotImplementedException("There is no such criteria for file viewing!");
+            }
+
+            if (tasksToView.Count == 0)
+            {
+                throw new ApplicationException("There are no tasks to view");
+            }
+
+            return tasksToView;
+        }
+
+        public override List<Task> Undo()
+        {
+            return screenState;
+        }
+    }
+}
