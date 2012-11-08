2af5515d158bf9786412ea0f38630b5b844e3551
Nizra <Nizra@Nizra-PC.nus.edu.sg>
2012-11-08 20:36:51 +0800

    Collate

--- WhiteBoard/WhiteboardTest/CommandParserTest.cs
+++ WhiteBoard/WhiteboardTest/CommandParserTest.cs
@@ -1,13 +1,11 @@
-﻿//@author U094776M
-
-using WhiteBoard;
+﻿using WhiteBoard;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 
 namespace WhiteboardTest
 {
-    
+    //@author U094776M
     
     /// <summary>
     ///This is a test class for CommandParserTest and is intended
@@ -76,13 +74,28 @@ namespace WhiteboardTest
         {
             //PrivateObject param0 = null; // TODO: Initialize to an appropriate value
             CommandParser_Accessor target = new CommandParser_Accessor(); // TODO: Initialize to an appropriate value
-            string usercommand = "hello    there     Jim"; // TODO: Initialize to an appropriate value
+
+            //test case 1
+            string usercommand = "  hello    there     Jim"; // TODO: Initialize to an appropriate value
             List<string> expected = new List<string>();
             expected.Add("hello");
             expected.Add("there");
             expected.Add("Jim");
             target.SplitString(usercommand);
             List<string> userCommandList = target.ReturnUserCommandListForTesting();
+            CollectionAssert.AreEqual(expected, userCommandList);
+            userCommandList.Clear();
+
+            //test case 2
+
+            usercommand = "  hello    there     Jim   76567 "; // TODO: Initialize to an appropriate value
+            expected = new List<string>();
+            expected.Add("hello");
+            expected.Add("there");
+            expected.Add("Jim");
+            expected.Add("76567");
+            target.SplitString(usercommand);
+            userCommandList = target.ReturnUserCommandListForTesting();
             CollectionAssert.AreEqual(expected, userCommandList);
         }
 
@@ -94,12 +107,15 @@ namespace WhiteboardTest
         public void IsValidTaskIdTest()
         {
             CommandParser_Accessor target = new CommandParser_Accessor(); // TODO: Initialize to an appropriate value
+
+            //Test case 1
             string str = "T00024"; // TODO: Initialize to an appropriate value
             int expected = 24; // TODO: Initialize to an appropriate value
             int actual;
             actual = target.IsValidTaskId(str);
             Assert.AreEqual(expected, actual);
-            //Assert.Inconclusive("Verify the correctness of this test method.");
+            
+            // Test case 2
         }
 
         /// <summary>
@@ -110,12 +126,15 @@ namespace WhiteboardTest
         public void IsTimeTest()
         {
             CommandParser_Accessor target = new CommandParser_Accessor(); // TODO: Initialize to an appropriate value
+
+            //Test case 1
             string time = "8pm"; // TODO: Initialize to an appropriate value
             bool expected = true; // TODO: Initialize to an appropriate value
             bool actual;
             actual = target.IsTime(time);
             Assert.AreEqual(expected, actual);
-           // Assert.Inconclusive("Verify the correctness of this test method.");
+           
+            // Test case 2
         }
 
         /// <summary>
@@ -126,12 +145,16 @@ namespace WhiteboardTest
         public void ParseTimeTest()
         {
             CommandParser_Accessor target = new CommandParser_Accessor(); // TODO: Initialize to an appropriate value
+
+            // Test case 1
             string time = "10:30pm"; // TODO: Initialize to an appropriate value
             TimeSpan expected = new TimeSpan(22,30,0); // TODO: Initialize to an appropriate value
             TimeSpan actual;
             actual = target.ParseTime(time);
             Assert.AreEqual(expected, actual);
-           // Assert.Inconclusive("Verify the correctness of this test method.");
+           
+
+            // Test case 2
         }
 
         /// <summary>
@@ -145,6 +168,28 @@ namespace WhiteboardTest
             bool expected = true; // TODO: Initialize to an appropriate value
             bool actual;
             actual = target.IsDate(datestring);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for ParseForDates
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("WhiteBoard.exe")]
+        public void ParseForDatesTest()
+        {
+            CommandParser_Accessor target = new CommandParser_Accessor(); // TODO: Initialize to an appropriate value
+            List<string> commandlist = new List<string>(); ; // TODO: Initialize to an appropriate value
+            commandlist.Add("Go");
+            commandlist.Add("shopping");
+            commandlist.Add("on");
+            commandlist.Add("10.12.12");
+            int index = 1; // TODO: Initialize to an appropriate value
+            bool modify = false; // TODO: Initialize to an appropriate value
+            int expected = 1; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.ParseForDates(commandlist, index, modify);
             Assert.AreEqual(expected, actual);
             //Assert.Inconclusive("Verify the correctness of this test method.");
         }
