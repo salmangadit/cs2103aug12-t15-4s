388541177628fa5ed2719e00a33d7eb93eb0eb5f
Asus <Asus@VARUN.nus.edu.sg>
2012-11-08 17:07:41 +0800

    1. Refactoring changes to DateHolder Class

---
+++ WhiteBoard/WhiteBoard/CommandHistory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhiteBoard
+{
+    class CommandHistory
+    {
+        List<string> commandHistory;
+        int currIndex = -1;
+
+        public CommandHistory()
+        {
+            commandHistory = new List<string>();
+        }
+
+        public void AddToHistory(string command)
+        {
+            if (command.Split(' ')[0].ToLower() == "search:")
+                return;
+
+            commandHistory.Add(command);
+            currIndex = -1;
+        }
+
+        public string UpClick()
+        {
+            if (commandHistory.Count == 0)
+                return null;
+
+            if (currIndex == -1)
+                currIndex = commandHistory.Count - 1;
+            else if (currIndex == 0)
+                return null;
+            else
+                currIndex -= 1;
+
+            return commandHistory[currIndex];
+        }
+
+        public string DownClick()
+        {
+            if (commandHistory.Count == 0)
+                return null;
+
+            if (currIndex == -1)
+                return null;
+            else if (currIndex == commandHistory.Count - 1)
+                return null;
+            else
+                currIndex += 1;
+
+            return commandHistory[currIndex];
+        }
+    }
+}
---
+++ WhiteBoard/WhiteBoard/DateHolder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhiteBoard
+{
+    class DateHolder
+    {
+        private string[] DAYS_OF_WEEK = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "TODAY", "TOMORROW" };
+        private const int TODAY_ID = 7;
+        private const int TOMORROW_ID = 8;
+        private string dateString;                      //String containing the date
+        private int dateId;                             //The ID denoting whether the date is in day or date form
+
+        public DateHolder(string datestring, int dateid)
+        {
+            dateString = datestring;
+            dateId = dateid;
+        }
+
+        public DateTime ConvertToDateTime()
+        {
+            if (dateId == 1)                            //If date is in a day form
+            {
+                int i = 0;
+                int dayid = -1;
+                int days_difference;
+                DayOfWeek today = DateTime.Now.DayOfWeek;
+                var day = today;
+                foreach (string str in DAYS_OF_WEEK)
+                {
+                    if (String.Equals(dateString, str, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        dayid = i;
+                    }
+                    i++;
+                }
+                if (dayid == TODAY_ID)
+                {
+                    day = today;
+                }
+                else if (dayid == TOMORROW_ID)
+                {
+                    day = today + 1;
+                }
+                else
+                {
+                    day = (DayOfWeek)dayid;
+                }
+                var days = day - today;
+                days_difference = (int)days;
+                DateTime temp_date = DateTime.Now.AddDays(days_difference);
+                if ((days_difference < 0 || days_difference == 0) && dayid != 7)
+                {
+                    temp_date = temp_date.AddDays(7);   //Denotes that the day belongs to the following week
+                }
+                return temp_date;
+            }
+
+            else
+            {
+                try
+                {
+                    var cultureinfo = new System.Globalization.CultureInfo("en-gb");
+                    DateTime.Parse(dateString, cultureinfo, System.Globalization.DateTimeStyles.None);
+                    return DateTime.Parse(dateString);
+                }
+                catch(FormatException e)
+                {
+                    throw e;
+                }
+            }
+        }
+    }
+}
